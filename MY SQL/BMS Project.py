92e8bf1150164076b4c3c16aaabf6dd0707aad3a
Ni9Logic <rehmanhassan117@gmail.com>
2021-07-17 21:19:25 +0500

    Update BMS Project.py

--- MY SQL/BMS Project.py
+++ MY SQL/BMS Project.py
@@ -8,7 +8,7 @@ from re import I
 db = mysql.connector.connect(host="localhost", user="root", passwd="root", database="banking")
 database = db.cursor()
 
-# I am gonna learn & implement mySQL on this now!
+# I am gonna learn & implement mySQL on this now! #DONE...
 class SignInPage:
     user_choice = "1"
     admin_name = "0"
@@ -85,7 +85,10 @@ class NewUser:
     def new_user_createaccount(self):
             os.system("cls||clear")
             select_query = "SELECT * FROM user WHERE name=%s"
-            self.new_user_name = input("\t\t\tEnter\u001b[1;31m Username\u001b[1;0m: ")
+            self.new_user_name = "0"
+            if self.new_user_name == "0" or len(self.new_user_name) == 0:
+                while self.new_user_name == "0" or len(self.new_user_name) == 0:
+                    self.new_user_name = input("\t\t\tEnter\u001b[1;31m Username\u001b[1;0m: ")
             database.execute(select_query, (self.new_user_name,))
             if database.fetchall():
                 print("\t\t\tUser Alreasdy Exists")
@@ -93,11 +96,27 @@ class NewUser:
                 m.getch()
             else:
                 self.created = datetime.now()
-                self.new_user_age = input("\t\t\tEnter\u001b[1;31m Age\u001b[1;0m of the user: ")
-                self.new_user_pass = input("\t\t\tEnter\u001b[1;31m Password\u001b[1;0m For User: ")
-                self.new_user_BankBal = input("\t\t\tEnter\u001b[1;31m Initial_Deposit\u001b[1;0m For User: ")
+                self.new_user_age = "0"
+                if self.new_user_age == "0" or len(self.new_user_age) == 0:
+                    while self.new_user_age == "0" or len(self.new_user_age) == 0:
+                        self.new_user_age = input("\t\t\tEnter\u001b[1;31m Age\u001b[1;0m: ")
+                self.new_user_pass = "0"
+                if self.new_user_pass == "0" or len(self.new_user_pass) == 0:
+                    while self.new_user_pass == "0" or len(self.new_user_pass) == 0:
+                        self.new_user_pass = input("\t\t\tEnter\u001b[1;31m Password\u001b[1;0m For User: ")
+                self.new_user_BankBal = "0"
+                if self.new_user_BankBal == "0" or len(self.new_user_BankBal) == 0:
+                    while self.new_user_BankBal == "0" or len(self.new_user_BankBal) == 0:
+                        self.new_user_BankBal = input("\t\t\tEnter\u001b[1;31m Initial_Deposit\u001b[1;0m For User: ")
                 print("\t\t\tThere are \u001b[1;31mTwo Types\u001b[1;0m of Banking Accounts\n\t\t\t1 \u001b[1;31m-->\u001b[1;0m Savings \n\t\t\t2 \u001b[1;31m-->\u001b[1;0m Current")
-                self.new_user_Account_Type = input("\t\t\tEnter\u001b[1;31m Account_Type\u001b[1;0m For User: ")
+                self.new_user_Account_Type = "0"
+                if self.new_user_Account_Type == "0" or len(self.new_user_Account_Type) == 0:
+                    while self.new_user_Account_Type == "0" or len(self.new_user_Account_Type) == 0:
+                        self.new_user_Account_Type = input("\t\t\tEnter\u001b[1;31m Account_Type\u001b[1;0m For User: ")
+                        if self.new_user_Account_Type == "1":
+                            self.new_user_Account_Type == "Savings"
+                        elif self.new_user_Account_Type == "2":
+                            self.new_user_Account_Type == "Current"
                 print("\t\t\tAccount \u001b[1;31mSuccessfully Created\u001b[1;0m\n\t\t\tPress Any Key to continue")
                 m.getch()
                 # Uploading into database
@@ -265,7 +284,7 @@ class Admin(SignInPage, NewUser): #ADMIN CLASS DONE!
             print("\t\t\tUser \u001b[1;31mNOT Found\u001b[1;0m\n")
             print("\t\t\tPress \u001b[1;31mANY\u001b[1;0m key to continue\n")
             m.getch()
-class User(SignInPage, NewUser):
+class User(SignInPage, NewUser): #USER CLASS DONE!
     user_choice = "0"
 
     def usermenu(self):
